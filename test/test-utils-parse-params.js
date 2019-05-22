18cec1a196d90b9e41e38b91478b8d50cc16fb11
vladislao <netcemetery@yandex.ru>
2019-05-22 14:05:17 +0400

    Add information about decoded value
    Closes #203

--- test/test-utils-parse-params.js
+++ test/test-utils-parse-params.js
@@ -28,63 +28,63 @@ var group = path.basename(__filename, '.js') + '/';
     what: 'Subtype with asterisk'
   },
   { source: 'text/plain; encoding=utf8',
-    expected: ['text/plain', ['encoding', 'utf8']],
+    expected: ['text/plain', ['encoding', ['utf8', false]]],
     what: 'Unquoted'
   },
   { source: 'text/plain; encoding=',
-    expected: ['text/plain', ['encoding', '']],
+    expected: ['text/plain', ['encoding', ['', false]]],
     what: 'Unquoted empty string'
   },
   { source: 'text/plain; encoding="utf8"',
-    expected: ['text/plain', ['encoding', 'utf8']],
+    expected: ['text/plain', ['encoding', ['utf8', false]]],
     what: 'Quoted'
   },
   { source: 'text/plain; greeting="hello \\"world\\""',
-    expected: ['text/plain', ['greeting', 'hello "world"']],
+    expected: ['text/plain', ['greeting', ['hello "world"', false]]],
     what: 'Quotes within quoted'
   },
   { source: 'text/plain; encoding=""',
-    expected: ['text/plain', ['encoding', '']],
+    expected: ['text/plain', ['encoding', ['', false]]],
     what: 'Quoted empty string'
   },
   { source: 'text/plain; encoding="utf8";\t   foo=bar;test',
-    expected: ['text/plain', ['encoding', 'utf8'], ['foo', 'bar'], 'test'],
+    expected: ['text/plain', ['encoding', ['utf8', false]], ['foo', ['bar', false]], 'test'],
     what: 'Multiple params with various spacing'
   },
   { source: "text/plain; filename*=iso-8859-1'en'%A3%20rates",
-    expected: ['text/plain', ['filename', '£ rates']],
+    expected: ['text/plain', ['filename', ['£ rates', true]]],
     what: 'Extended parameter (RFC 5987) with language'
   },
   { source: "text/plain; filename*=utf-8''%c2%a3%20and%20%e2%82%ac%20rates",
-    expected: ['text/plain', ['filename', '£ and € rates']],
+    expected: ['text/plain', ['filename', ['£ and € rates', true]]],
     what: 'Extended parameter (RFC 5987) without language'
   },
   { source: "text/plain; filename*=utf-8''%E6%B5%8B%E8%AF%95%E6%96%87%E6%A1%A3",
-    expected: ['text/plain', ['filename', '测试文档']],
+    expected: ['text/plain', ['filename', ['测试文档', true]]],
     what: 'Extended parameter (RFC 5987) without language #2'
   },
   { source: "text/plain; filename*=iso-8859-1'en'%A3%20rates; altfilename*=utf-8''%c2%a3%20and%20%e2%82%ac%20rates",
-    expected: ['text/plain', ['filename', '£ rates'], ['altfilename', '£ and € rates']],
+    expected: ['text/plain', ['filename', ['£ rates', true]], ['altfilename', ['£ and € rates', true]]],
     what: 'Multiple extended parameters (RFC 5987) with mixed charsets'
   },
   { source: "text/plain; filename*=iso-8859-1'en'%A3%20rates; altfilename=\"foobarbaz\"",
-    expected: ['text/plain', ['filename', '£ rates'], ['altfilename', 'foobarbaz']],
+    expected: ['text/plain', ['filename', ['£ rates', true]], ['altfilename', ['foobarbaz', false]]],
     what: 'Mixed regular and extended parameters (RFC 5987)'
   },
   { source: "text/plain; filename=\"foobarbaz\"; altfilename*=iso-8859-1'en'%A3%20rates",
-    expected: ['text/plain', ['filename', 'foobarbaz'], ['altfilename', '£ rates']],
+    expected: ['text/plain', ['filename', ['foobarbaz', false]], ['altfilename', ['£ rates', true]]],
     what: 'Mixed regular and extended parameters (RFC 5987) #2'
   },
   { source: 'text/plain; filename="C:\\folder\\test.png"',
-    expected: ['text/plain', ['filename', 'C:\\folder\\test.png']],
+    expected: ['text/plain', ['filename', ['C:\\folder\\test.png', false]]],
     what: 'Unescaped backslashes should be considered backslashes'
   },
   { source: 'text/plain; filename="John \\"Magic\\" Smith.png"',
-    expected: ['text/plain', ['filename', 'John "Magic" Smith.png']],
+    expected: ['text/plain', ['filename', ['John "Magic" Smith.png', false]]],
     what: 'Escaped double-quotes should be considered double-quotes'
   },
   { source: 'multipart/form-data; charset=utf-8; boundary=0xKhTmLbOuNdArY',
-    expected: ['multipart/form-data', ['charset', 'utf-8'], ['boundary', '0xKhTmLbOuNdArY']],
+    expected: ['multipart/form-data', ['charset', ['utf-8', false]], ['boundary', ['0xKhTmLbOuNdArY', false]]],
     what: 'Multiple non-quoted parameters'
   },
 ].forEach(function(v) {
